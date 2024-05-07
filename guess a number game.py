02c7ecf8c23e3c5ed82103f65532305c89e7d6db
Lilac Utz <166491894+LilacUtz@users.noreply.github.com>
2024-05-07 13:19:56 +0300

    Add files via upload

---
+++ guess a number game.py
@@ -0,0 +1,47 @@
+
+from random import randint
+
+EASY_LEVEL = 10
+HARD_LEVEL = 5
+
+
+def check_answer(user_guess, answer, turns):
+    if user_guess > answer:
+        print("Too high")
+        return turns - 1
+    elif user_guess < answer:
+        print("Too low")
+        return turns - 1
+    elif user_guess == answer:
+        print(f"You win! the answer is {answer}")
+
+
+
+
+def game_level():
+    level = input("Choose a level, Type 'easy' or 'hard': ")
+    if level == "easy":
+       return EASY_LEVEL
+    else:
+        return HARD_LEVEL
+    
+
+def game_on()  :
+    answer = randint(1, 100)
+    print(f"the answer is {answer}")
+    print("You need to guess a number between 1 and 100")
+    turns = game_level()
+
+    user_guess = 0
+    while user_guess != answer:
+        print(f"You have {turns} left")
+        user_guess = int(input("Make a guess: "))
+        turns = check_answer(user_guess, answer, turns)
+        if turns == 0 :
+            print("You have no more guesses left. You lost.")
+            return
+        elif user_guess != answer:
+            print("Guess again...")
+
+
+game_on()
